18f732b3d8133550b677a2cadde7fb4c758cc6bb
ko-kamenashi <kamenashi0917@gmail.com>
2021-03-01 09:15:55 +0900

    feat: added test code and delete example code
    1.2

--- src/test/scala/learn/tdd/money/DollarTest.scala
+++ src/test/scala/learn/tdd/money/DollarTest.scala
@@ -1,29 +1,30 @@
 package learn.tdd.money
 
 import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.concurrent.TimeLimits.failAfter
-import org.scalatest.time.SpanSugar._
-import org.mockito.Mockito.{mock, when}
 
 /** TODO: list
- *  $5 +10 CHF = $10 (When the rate is 2: 1)s
- *  [x] $5 * 2 = $10
- *  Change amount states to private
- *  What to do with Dollar's side effects?
- *  What to do Money's rounding?
+ * [ ] $5 +10 CHF = $10 (When the rate is 2: 1)s
+ * [x] $5 * 2 = $10
+ * [ ] Change amount states to private
+ * [x] What to do with Dollar's side effects?
+ * [ ] What to do Money's rounding?
  */
 
 class DollarTest extends AnyFlatSpec {
 
   /** There are 4 errors of compile..
-   *  Done: No Dollar class.
-   *  Done: No Constructor.
-   *  Done: No times Method.
-   *  Done: No amount Field.
+   * [x] No Dollar class.
+   * [x] No Constructor.
+   * [x] No times Method.
+   * [x] No amount Field.
    */
   val five = new Dollar(5)
 
-  "Dollar.sum function" should "multiple * amount = expected value" in {
+  "Dollar.sum function" should "multiple * amount = expected value 10" in {
     assert(five.times(2) === 10)
+  }
+
+  "Dollar.sum function" should "multiple * amount = expected value 15" in {
+    assert(five.times(3) === 15)
   }
 }
