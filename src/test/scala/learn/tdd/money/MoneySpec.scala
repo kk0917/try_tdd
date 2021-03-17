7a381193282d0da96217021ed604986bd1c09960
ko-kamenashi <kamenashi0917@gmail.com>
2021-03-17 19:11:10 +0900

    test step5
    5. Do refactoring and remove the duplicates
    -> I already did refactoring the last commits at Dollar.class: L7
    -> amount = amount * multiplier
    -> amount *= multiplier

--- src/test/scala/learn/tdd/money/MoneySpec.scala
+++ src/test/scala/learn/tdd/money/MoneySpec.scala
@@ -6,7 +6,7 @@ import org.scalatest.GivenWhenThen
 class MoneySpec extends AnyFeatureSpec with GivenWhenThen {
   /** TODO:
    * [ ] $5 + 10CHF = $10 (when rate is 2:1 between $ and CHF)
-   * [ ] $5 * 2 = $10
+   * [x] $5 * 2 = $10
    * [ ] change amount state to private?
    * [ ] What to do with Dollar's side effect?
    * [ ] What to do with Money's rounding?
