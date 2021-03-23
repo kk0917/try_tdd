bc0e96cb4b3c3c09014816abfb63bcb72323e3d2
ko-kamenashi <kamenashi0917@gmail.com>
2021-03-23 21:07:21 +0900

    chore: forgot to check
    1.2

--- src/test/scala/learn/tdd/money/MoneySpec.scala
+++ src/test/scala/learn/tdd/money/MoneySpec.scala
@@ -8,7 +8,7 @@ class MoneySpec extends AnyFeatureSpec with GivenWhenThen {
    * [ ] $5 + 10CHF = $10 (when rate is 2:1 between $ and CHF)
    * [x] $5 * 2 = $10
    * [ ] change amount state to private?
-   * [ ] What to do with Dollar's side effect?
+   * [x] What to do with Dollar's side effect?
    * [ ] What to do with Money's rounding?
    */
   info("Multilateral currency")
