73b4d8eeb6c04d56fb14a975741829df3ca70a25
ko-kamenashi <kamenashi0917@gmail.com>
2021-04-04 16:28:45 +0900

    chore: add default 2nd argument of Franc class
    forgot

--- src/main/scala/learn/tdd/money/Franc.scala
+++ src/main/scala/learn/tdd/money/Franc.scala
@@ -2,7 +2,7 @@ package learn.tdd.money
 
 class Franc(
   val amount: Int,
-  val currencyType: String
+  val currencyType: String = "CHF"
 ) extends Money {
 
   def times(multiplier: Int): Money = Money.franc(amount * multiplier)
