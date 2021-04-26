f4153d9bf61051ce6b7ed44f94fc10494b95aac0
ko-kamenashi <kamenashi0917@gmail.com>
2021-04-26 23:40:48 +0900

    feat: define Pair class etc

--- src/test/scala/learn/tdd/money/MoneySpec.scala
+++ src/test/scala/learn/tdd/money/MoneySpec.scala
@@ -118,13 +118,17 @@ class MoneySpec extends AnyFeatureSpec with GivenWhenThen {
   }
 
   Feature("1.14") {
-    Scenario("Recuce Money Different Currency") {
+    Scenario("Reduce Money Different Currency") {
       val bank: Bank = new Bank()
       bank.addRate("CHF", "USD", 2)
       val result: Money = bank.reduce(Money.franc(2), "USD")
 
       assert(Money.dollar(1).equals(result) === true)
     }
+
+//    Scenario("Array Equals") {
+//      assert((new Object {"abc"}).equals(new Object {"abc"}) === true)
+//    }
   }
   /** Summary
    *
